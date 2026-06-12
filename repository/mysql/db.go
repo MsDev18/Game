f1a0571402ba6af8a179f18385a5c2bc1a3273c2
MsDev18 <m.sadegh0086@gmail.com>
2026-06-12 23:34:58 +0800

    Watched S-11 || E-02

--- repository/mysql/db.go
+++ repository/mysql/db.go
@@ -1,6 +1,29 @@
 package mysql
 
-type DB struct {
+import (
+	"database/sql"
+	"fmt"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type MySQLDB struct {
+	db *sql.DB
 }
 
+func New() *MySQLDB {
+	db, err := sql.Open("mysql", "game:game-pass@(localhost:3307)/game")
+	if err != nil {
+		panic(fmt.Errorf("cant't open mysql db: %v", err))
+	}
+
+	// see important settings section
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return &MySQLDB{
+		db: db,
+	}
+}
