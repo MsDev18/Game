3a498220e129459f18f40150be6f41b3739ed025
MsDev18 <m.sadegh0086@gmail.com>
2026-06-21 19:28:34 +0330

    Watched To S-11 || E-03 || 1:10:30 Minutes

--- main.go
+++ main.go
@@ -1,47 +1,70 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"game/entity"
 	"game/repository/mysql"
+	"game/service/userservice"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/users/register" , userRegisterHandler)
-	err := http.ListenAndServe(":8081", nil)
-	fmt.Println("err: ", err)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health-check", healthCheckHandler)
+	mux.HandleFunc("/users/register", userRegisterHandler)
+
+	log.Println("server is listening on port 8081...")
+	err := http.ListenAndServe(":8081", mux)
+	if err != nil {
+		panic(err)
+	}
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: mux,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
-
-
 
 // ServeHTTP(ResponseWriter, *Request)
-func userRegisterHandler (writer http.ResponseWriter , req *http.Request) {
-	fmt.Println("Here..")
+func userRegisterHandler(writer http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
-		fmt.Fprintf(writer , "invalid method")
+		fmt.Fprintf(writer, `{"error" : "invalid method"}`)
 	}
+	
+	data, err := io.ReadAll(req.Body)
+	if err != nil {
+		writer.Write([]byte(fmt.Sprintf(`{"error" : "%s"}`, err.Error())))
+
+		return
+	}
+
+	var uReq userservice.RegisterRequest
+
+	err = json.Unmarshal(data , &uReq)
+	if err != nil {
+		writer.Write([]byte(fmt.Sprintf(`{"error" : "%s"}`, err.Error())))
+
+		return
+	}
+	fmt.Println(uReq)
+	mysqlRepo := mysql.New()
+	userSvc := userservice.New(mysqlRepo)
+
+	_, err = userSvc.Register(uReq)
+	if err != nil {
+		writer.Write([]byte(fmt.Sprintf(`{"error" : "%s"}`, err.Error())))
+
+		return
+	}
+	writer.Write([]byte(`{"message" : "user created !!!"}`))
 }
 
-func tetsUserMysqlRepo() {
-	mysqlRepo := mysql.New()
-
-	createdUser, err := mysqlRepo.Register(entity.User{
-		ID:          0,
-		Name:        "MsDev18",
-		PhoneNumber: "09351721410",
-	})
-
-	if err != nil {
-		fmt.Printf("register user: %v \n", err)
-	} else {
-		fmt.Printf("created user: %v\n", createdUser)
-	}
-	isUnique, err := mysqlRepo.IsPhoneNumberUnique(createdUser.PhoneNumber + "5")
-
-	if err != nil {
-		fmt.Printf("unique err: %v", err)
-	}
-	fmt.Println("isUnique -> ", isUnique)
+func healthCheckHandler(writer http.ResponseWriter, req *http.Request) {
+	
+	fmt.Fprintf(writer, `{"message" : "everything is good !"}`)
 }
